80cce89cda8d910aa0c30a2bcb6efeb6b4409074
Jess francis Partusa <128569963+francisphi@users.noreply.github.com>
2023-12-20 15:11:15 +0800

    Add files via upload

--- javascript/react.js
+++ javascript/react.js
@@ -1,1 +1,242 @@
+let result = document.getElementById("button");
+let result1 = document.getElementById("button1");
+let result2 = document.getElementById("button2");
+let result3 = document.getElementById("button3");
+let result4 = document.getElementById("button4");
 
+/* 1st react */
+function like() {
+  if (result.innerHTML === '<img src="emoji/like.gif">') {
+    result.innerHTML = "React";
+  } else {
+    result.innerHTML = '<img src="emoji/like.gif">';
+  }
+}
+
+function love() {
+  if (result.innerHTML === '<img src="emoji/heart.gif">') {
+    result.innerHTML = "React";
+  } else {
+    result.innerHTML = '<img src="emoji/heart.gif">';
+  }
+}
+
+function haha() {
+  if (result.innerHTML === '<img src="emoji/haha.gif">') {
+    result.innerHTML = "React";
+  } else {
+    result.innerHTML = '<img src="emoji/haha.gif">';
+  }
+}
+
+function wow() {
+  if (result.innerHTML === '<img src="emoji/wow1.gif">') {
+    result.innerHTML = "React";
+  } else {
+    result.innerHTML = '<img src="emoji/wow1.gif">';
+  }
+}
+
+function sad() {
+  if (result.innerHTML === '<img src="emoji/sad.gif">') {
+    result.innerHTML = "React";
+  } else {
+    result.innerHTML = '<img src="emoji/sad.gif">';
+  }
+}
+
+function angry() {
+  if (result.innerHTML === '<img src="emoji/angry.gif">') {
+    result.innerHTML = "React";
+  } else {
+    result.innerHTML = '<img src="emoji/angry.gif">';
+  }
+}
+
+
+function like1() {
+  if (result1.innerHTML === '<img src="emoji/like.gif">') {
+    result1.innerHTML = "React";
+  } else {
+    result1.innerHTML = '<img src="emoji/like.gif">';
+  }
+}
+
+function love1() {
+  if (result1.innerHTML === '<img src="emoji/heart.gif">') {
+    result1.innerHTML = "React";
+  } else {
+    result1.innerHTML = '<img src="emoji/heart.gif">';
+  }
+}
+
+function haha1() {
+  if (result1.innerHTML === '<img src="emoji/haha.gif">') {
+    result1.innerHTML = "React";
+  } else {
+    result1.innerHTML = '<img src="emoji/haha.gif">';
+  }
+}
+
+function wow1() {
+  if (result1.innerHTML === '<img src="emoji/wow1.gif">') {
+    result1.innerHTML = "React";
+  } else {
+    result1.innerHTML = '<img src="emoji/wow1.gif">';
+  }
+}
+
+function sad1() {
+  if (result1.innerHTML === '<img src="emoji/sad.gif">') {
+    result1.innerHTML = "React";
+  } else {
+    result1.innerHTML = '<img src="emoji/sad.gif">';
+  }
+}
+
+function angry1() {
+  if (result1.innerHTML === '<img src="emoji/angry.gif">') {
+    result1.innerHTML = "React";
+  } else {
+    result1.innerHTML = '<img src="emoji/angry.gif">';
+  }
+}
+
+/* 3rd react */
+function like2() {
+  if (result2.innerHTML === '<img src="emoji/like.gif">') {
+    result2.innerHTML = "React";
+  } else {
+    result2.innerHTML = '<img src="emoji/like.gif">';
+  }
+}
+
+function love2() {
+  if (result2.innerHTML === '<img src="emoji/heart.gif">') {
+    result2.innerHTML = "React";
+  } else {
+    result2.innerHTML = '<img src="emoji/heart.gif">';
+  }
+}
+
+function haha2() {
+  if (result2.innerHTML === '<img src="emoji/haha.gif">') {
+    result2.innerHTML = "React";
+  } else {
+    result2.innerHTML = '<img src="emoji/haha.gif">';
+  }
+}
+
+function wow2() {
+  if (result2.innerHTML === '<img src="emoji/wow1.gif">') {
+    result2.innerHTML = "React";
+  } else {
+    result2.innerHTML = '<img src="emoji/wow1.gif">';
+  }
+}
+
+function sad2() {
+  if (result2.innerHTML === '<img src="emoji/sad.gif">') {
+    result2.innerHTML = "React";
+  } else {
+    result2.innerHTML = '<img src="emoji/sad.gif">';
+  }
+}
+
+function angry2() {
+  if (result2.innerHTML === '<img src="emoji/angry.gif">') {
+    result2.innerHTML = "React";
+  } else {
+    result2.innerHTML = '<img src="emoji/angry.gif">';
+  }
+}
+
+/* 4th react */
+function like3() {
+  if (result3.innerHTML === '<img src="emoji/like.gif">') {
+    result3.innerHTML = "React";
+  } else {
+    result3.innerHTML = '<img src="emoji/like.gif">';
+  }
+}
+
+function love3() {
+  if (result3.innerHTML === '<img src="emoji/heart.gif">') {
+    result3.innerHTML = "React";
+  } else {
+    result3.innerHTML = '<img src="emoji/heart.gif">';
+  }
+}
+
+function haha3() {
+  if (result3.innerHTML === '<img src="emoji/haha.gif">') {
+    result3.innerHTML = "React";
+  } else {
+    result3.innerHTML = '<img src="emoji/haha.gif">';
+  }
+}
+
+function wow3() {
+  if (result3.innerHTML === '<img src="emoji/wow1.gif">') {
+    result3.innerHTML = "React";
+  } else {
+    result3.innerHTML = '<img src="emoji/wow1.gif">';
+  }
+}
+
+function sad3() {
+  if (result3.innerHTML === '<img src="emoji/sad.gif">') {
+    result3.innerHTML = "React";
+  } else {
+    result3.innerHTML = '<img src="emoji/sad.gif">';
+  }
+}
+
+function angry3() {
+  if (result3.innerHTML === '<img src="emoji/angry.gif">') {
+    result3.innerHTML = "React";
+  } else {
+    result3.innerHTML = '<img src="emoji/angry.gif">';
+  }
+}
+
+/* 5th react */
+function like4() {
+  if (result4.innerHTML === '<img src="emoji/like.gif">') {
+    result4.innerHTML = "React";
+  } else {
+    result4.innerHTML = '<img src="emoji/like.gif">';
+  }
+}
+
+function love4() {
+  if (result4.innerHTML === '<img src="emoji/heart.gif">') {
+    result4.innerHTML = "React";
+  } else {
+    result4.innerHTML = '<img src="emoji/heart.gif">';
+  }
+}
+
+function haha4() {
+  if (result4.innerHTML === '<img src="emoji/haha.gif">') {
+    result4.innerHTML = "React";
+  } else {
+    result4.innerHTML = '<img src="emoji/haha.gif">';
+  }
+}
+
+function wow4() {
+  if (result4.innerHTML === '<img src="emoji/wow1.gif">') {
+    result4.innerHTML = "React";
+  } else {
+    result4.innerHTML = '<img src="emoji/wow1.gif">';
+  }
+}
+
+function angry4() {
+  if (result3.innerHTML === '<img src="emoji/angry.gif">') {
+    result4.innerHTML = "React";
+  } else {
+    result4.innerHTML = '<img src="emoji/angry.gif">';
+  }
+}
